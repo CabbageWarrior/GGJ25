c2c9b3cc65394b93b0377022a502783fcafe464e
Verza-ROG <feelthecabbage@gmail.com>
2025-01-25 16:27:22 +0100

    Working on nest.

--- Assets/0_Project/Scripts/Managers/ScoreManager.cs
+++ Assets/0_Project/Scripts/Managers/ScoreManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private GameSelector gameSelector;
+    [SerializeField] private NormalBubble[] bubblePivots;
 
     private int[] slots = { 0, 0, 0, 0, 0, 0 };
 
@@ -16,6 +17,9 @@ public class ScoreManager : MonoBehaviour
             if (slots[i] == 0)
             {
                 slots[i] = step;
+
+                bubblePivots[i].SetState(step);
+
                 break;
             }
         }
